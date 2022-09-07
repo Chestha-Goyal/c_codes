1016b493d79120812c27e535bcbe77340803bf70
Chestha-Goyal <78256898+Chestha-Goyal@users.noreply.github.com>
2022-09-07 10:39:26 +0530

    Add files via upload

---
+++ loop_in_array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string names[4] = {"Rohit", "Gagan", "Mohit", "Rakesh"};
+  for (int i = 0; i < 4; i++) {
+    cout << names[i] <<" ";
+  }
+  return 0;
+}

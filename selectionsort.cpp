f11cba319bc0372a058b67d3678bcfd4ee8502a3
Chestha Goyal <chesthagoyal0000@gmail.com>
2022-09-15 10:50:24 +0530

    Add files via upload

---
+++ selectionsort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+ using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number: "<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements in array: "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=1; i<n-1; i++)
+    {
+       int min=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[min])
+            {
+                min=j;
+            }
+        }
+        int temp=arr[i];
+        arr[i]=arr[min];
+        arr[min]=temp;
+    }
+    cout<<"Sorted array: "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}

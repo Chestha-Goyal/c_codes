a5ece8a3b17709a5ab142ada4296d82e2f178531
Chestha Goyal <chesthagoyal0000@gmail.com>
2022-10-06 23:41:22 +0530

    Add files via upload

---
+++ shallowcopy.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Test
+{
+private:
+  int x;
+public:			//whenever we don't place any constructor by deafult it will automatically complier will place this
+  //Test() {}  //Automatic default constructor
+  //default constructor
+    Test ()
+  {
+    x = 5;
+  }
+  //parameterized constructor
+  Test(int a)
+  {
+      x = a;
+  }
+  //Copy constructor
+  Test(Test &t)
+  {
+      x = t.x;
+  }
+  void setX ()
+  {
+    cout << "Enter X: ";
+    cin >> x;
+  }
+  int getX ()
+  {
+    return x;
+  }
+};
+
+
+int main ()                 //public me rkhna hai constructor
+{
+  Test ob;			//implicitly invokes constructor
+  Test ob1(25);		// invoke parameterized constructor
+  Test ob2(ob1);    // copy constructor
+  cout<<"Data: "<< ob.getX()<<endl;
+  cout<<"Data: "<< ob1.getX()<<endl;
+  cout<<"Data: "<< ob2.getX()<<endl;    
+  ob.setX();
+  ob1.setX();
+  cout<<"Data: "<<ob.getX()<<endl;
+  cout<<"Data: "<<ob1.getX()<<endl;
+  cout<<"Data: "<<ob2.getX()<<endl;
+  
+  Test ob3 = ob1; //shallow copy // invokes copy constructor
+  cout<<"Data: "<<ob3.getX()<<endl;
+  ob1.setX();
+  cout<<"Data: "<<ob3.getX()<<endl;  //there is no chnge in value (deep copy)
+  return 0;
+

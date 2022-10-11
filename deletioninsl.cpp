78c8e03d1a7ed4fc381cd55e5644d527f4a318d0
Chestha Goyal <chesthagoyal0000@gmail.com>
2022-10-12 01:38:49 +0530

    Add files via upload

---
+++ deletioninsl.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+class Node {
+
+    public:
+    int data;
+    Node* next;
+
+    //constructor
+    Node(int data) {
+        this -> data = data;
+        this -> next = NULL;
+    }
+
+    //destructor
+    ~Node() {
+        int value = this -> data;
+        //memory free
+        if(this->next != NULL) {
+            delete next;
+            this->next = NULL;
+        }
+        cout << " memory is free for node with data " << value << endl;
+    }
+
+};
+
+void deleteNode(int position, Node* & head) { 
+
+    //deleting first or start node
+    if(position == 1) {
+        Node* temp = head;
+        head = head -> next;
+        //memory free start ndoe
+        temp -> next = NULL;
+        delete temp;
+    }
+    else
+    {
+        //deleting any middle node or last node
+        Node* curr = head;
+        Node* prev = NULL;
+
+        int cnt = 1;
+        while(cnt < position) {
+            prev = curr;
+            curr = curr -> next;
+            cnt++;
+        }
+
+        prev -> next = curr -> next;
+        curr -> next  = NULL;
+        delete curr;
+
+    }
+}
+
+int main() {
+    
+    //created a new node
+    Node* node1 = new Node(10);
+    
+    //head pointed to node1
+    Node* head = node1; 
+    Node* tail = node1;
+
+    //deleteNode(4, head);
+    
+
+    tail -> next = head ->next;
+
+    cout << "head " << head -> data << endl;
+    cout << "tail " << tail -> data << endl;
+
+}
